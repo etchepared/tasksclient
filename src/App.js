c477c4512eb330c3297f927f754cbac967dfec93
etchepared <etcheparede@gmail.com>
2022-08-21 21:11:35 -0300

    Added Dashboard.jsx, Login.jsx, Projects.jsx, Reports.jsx and Staff.jsx components, also their app routes has been created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Projects from "./pages/Projects";
+import Reports from "./pages/Reports";
+import Staff from "./pages/Staff";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/staff" element={<Staff />} />
+      </Routes>
     </div>
   );
 }
