87fab9c4bfbd14fba4f278fb3e77f3edf4a10928
etchepared <etcheparede@gmail.com>
2022-08-25 19:04:46 -0300

    login html created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div>
+      <h1>Task Manager Login</h1>
+      <div class="inputs-container">
+        <div>
+          <input type="text" placeholder="usuario..." />
+        </div>
+        <div>
+          <input type="password" placeholder="contraseña..." />
+        </div>
+        <button type="submit">Ingresar</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
